720c2afa3189cda052105627af432fb31778f5a7
Jacob Dev <mercihabam@gmail.com>
2022-10-11 12:53:03 +0200

    fix tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#1D2130'
+        primary: '#1D2130',
+        maron: 'rgba(215,152,225,0.15)',
+        lightblue: 'rgba(224,228,252,0.5)'
       }
     },
   },
